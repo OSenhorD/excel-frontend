075108adb0ef10f5070ebe750bd6ae13c37e8b05
David Marques <davidmarques.261299@gmail.com>
2024-05-25 12:33:38 -0300

    refactor(): Ajuste do local dos menus

--- src/app/_layouts/default.component.ts
+++ src/app/_layouts/default.component.ts
@@ -1,7 +1,14 @@
-import { Component } from "@angular/core"
+import {
+  Component,
+  inject,
+  OnInit,
+} from "@angular/core"
+
 import { RouterModule } from "@angular/router"
 
-import { MenuComponent } from "src/app/components/menu.component"
+import { IMenu, MenuComponent } from "src/app/components/menu.component"
+
+import { AuthService, IResponseAuthUser } from "src/app/services/authenticate/auth.service"
 
 @Component({
   selector: "layout-default",
@@ -11,7 +18,7 @@ import { MenuComponent } from "src/app/components/menu.component"
     MenuComponent,
   ],
   template: `
-    <custom-menu></custom-menu>
+    <custom-menu [menus]="menus"></custom-menu>
 
     <div class="p-4 md:ml-64">
       <router-outlet></router-outlet>
@@ -19,5 +26,58 @@ import { MenuComponent } from "src/app/components/menu.component"
   `,
 })
 
-export class DefaultComponent {
+export class DefaultComponent implements OnInit {
+  private readonly _authService: AuthService = inject(AuthService)
+
+  protected menus: IMenu[] = []
+
+  ngOnInit(): void {
+    if (this.user.isAdmin) {
+      this.menus = [
+        {
+          id: "1",
+          label: "Home",
+          route: "home",
+          icon: "fa-solid fa-house",
+          children: [],
+        },
+        {
+          id: "2",
+          label: "Cadastro de Usuários",
+          route: "security/users",
+          icon: "fa-solid fa-users",
+          children: [],
+        },
+        {
+          id: "3",
+          label: "Dados",
+          route: "database/data",
+          icon: "fa-solid fa-address-book",
+          children: [],
+        },
+      ]
+      return
+    }
+
+    this.menus = [
+      {
+        id: "1",
+        label: "Home",
+        route: "home",
+        icon: "fa-solid fa-house",
+        children: [],
+      },
+      {
+        id: "3",
+        label: "Dados",
+        route: "database/data",
+        icon: "fa-solid fa-address-book",
+        children: [],
+      },
+    ]
+  }
+
+  protected get user(): IResponseAuthUser {
+    return this._authService.user.user || {}
+  }
 }
